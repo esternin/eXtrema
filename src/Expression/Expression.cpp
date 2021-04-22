esternin/eXtrema

commit 623e5a89fc1420cf920aeba0f1dccc59cea7e29c

Get rid of unnecessary <hash_map> includes
We never used this (deprecated, pre-C++11 standard) class anyhow and it
just resulted in a bunch of

	/usr/include/c++/8/backward/backward_warning.h:32:2: warning:
	#warning This file includes at least one deprecated or
	antiquated header which may be removed without further notice at
	a future date. Please use a non-deprecated interface with
	equivalent functionality instead. For a listing of replacement
	headers and interfaces, consult the file backward_warning.h. To
	disable this warning use -Wno-deprecated. [-Wcpp]

warnings.

src/Expression/Expression.cpp
@@ -17,14 +17,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 #include <memory>
 
-#if __GNUC__ > 4 || \
-  (__GNUC__ == 4 && (__GNUC_MINOR__ > 3 || \
-    (__GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ >= 0)))
-#include <hash_map>
-#else
-#include <ext/hash_map>
-#endif
-
 #include "Expression.h"
 #include "EExpressionError.h"
 #include "ExprCodes.h"

src/Parsing/Script.cpp
@@ -18,14 +18,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sstream>
 #include <stdexcept>
 
-#if __GNUC__ > 4 || \
-  (__GNUC__ == 4 && (__GNUC_MINOR__ > 3 || \
-    (__GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ >= 0)))
-#include <hash_map>
-#else
-#include <ext/hash_map>
-#endif
-
 #include "Script.h"
 #include "ESyntaxError.h"
 #include "ParseToken.h"

src/wxForms/GraphicsPage.cpp
@@ -16,13 +16,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#if __GNUC__ > 4 || \
-  (__GNUC__ == 4 && (__GNUC_MINOR__ > 3 || \
-    (__GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ >= 0)))
-#include <hash_map>
-#else
-#include <ext/hash_map>
-#endif
 #include <memory>
 
 #include "GraphicsPage.h"

commit 66d5e9aeffc0cc7dbab7d4929b8ebac129404d5a

Include <algorithm> to get std::find() declaration
This fixes the build after the changes of 623e5a8 (Get rid of
unnecessary <hash_map> includes, 2021-04-22) which accidentally removed
(indirect) inclusion of <algorithm> via the obsolete <hash_map> header.

Just include it explicitly now.

src/Expression/Expression.cpp
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+#include <algorithm>
 #include <memory>
 
 #include "Expression.h"

src/Parsing/Script.cpp
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+#include <algorithm>
 #include <sstream>
 #include <stdexcept>
 

src/wxForms/GraphicsPage.cpp
@@ -16,6 +16,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+#include <algorithm>
 #include <memory>
 
 #include "GraphicsPage.h"