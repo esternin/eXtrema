esternin/eXtrema

commit 9225c00e0cffdd59152d64af08819d39bc99e369

Avoid an apparently unnecessary refresh in CMD_density
The bitmaps are already drawn on the screen by the time
RefreshGraphics() was called and it seems unnecessary to refresh them
again.

src/Commands/CMD_density.cpp
@@ -492,7 +492,6 @@ void CMD_density::Execute( ParseLine const *p )
     }
     ExGlobals::GetGraphWindow()->AddDrawableObject( gp );
   }
-  ExGlobals::RefreshGraphics();
 }
 
 // end of file

commit ba9a960152dde7bc7ba907c923e592fe4aff08db

Restore RefreshGraphics() call in CMD_density::Execute()
Removing it in 9225c00 (Avoid an apparently unnecessary refresh in
CMD_density, 2021-04-22) broke background redrawing, as this is only
done during full repaint in GRA_window::Draw(), so restore this call
and, instead, to avoid drawing all the density plots twice, don't draw
them on screen at all in Execute() but only draw them on an off-screen
wxMemoryDC just to check that we get no errors while drawing.

Notice that this is still quite inefficient as we go through everything
(notably conversion to wxImage and wxBitmap) but actual drawing, and it
would be best to avoid doing this entirely, if possible.

src/Commands/CMD_density.cpp
@@ -390,7 +390,18 @@ void CMD_density::Execute( ParseLine const *p )
     y.assign( yi.begin(), yi.end() );
     z.assign( array.begin(), array.end() );
   }
-  wxClientDC dc( ExGlobals::GetwxWindow() );
+
+  // We don't need to actually draw anything here, this will be done later,
+  // after RefreshGraphics() is called at the end of this function. However we
+  // do want to call Draw() just to check that the new drawable object can be
+  // drawn successfully before storing it, so we create a dummy DC, using the
+  // smallest possible valid bitmap, only to be able to use it for checking
+  // this.
+  //
+  // IF we can ever prove that checking for EGraphicsError here is unnecessary,
+  // we should remove this DC entirely and not call Draw() at all from here.
+  wxBitmap dummyBitmap(1, 1);
+  wxMemoryDC dc(dummyBitmap);
   if( qualifiers[wxT("BOXES")] )
   {
     GRA_boxPlot *bp = new GRA_boxPlot( x, y, z, nrow, fmin, fmax, qmin, qmax,
@@ -492,6 +503,7 @@ void CMD_density::Execute( ParseLine const *p )
     }
     ExGlobals::GetGraphWindow()->AddDrawableObject( gp );
   }
+  ExGlobals::RefreshGraphics();
 }
 
 // end of file