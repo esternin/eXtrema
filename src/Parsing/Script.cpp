esternin/eXtrema

commit 1a3bde3de28949d81d1232416e78a56c6a7d5a28

Don't use the script after all scripts were stopped
Check if the script being run still exists after each command execution,
as it could be surreptitiously deleted if the command execution resulted
in a call to StopAllScripts(), as it happens in demo.pcm, for example.

Closes #8.

src/Parsing/Script.cpp
@@ -58,7 +58,17 @@ void Script::Run()
       if( echo_ )ExGlobals::WriteOutput( commandLine );
     }
     if( !commandLine.empty() && commandLine!=wxString(wxT(" ")) )
+    {
       ExGlobals::ProcessCommand(commandLine);
+      if ( ExGlobals::NotInaScript() )
+      {
+        // This can only happen if StopAllScripts() was called from inside the
+        // command, in which case this script object itself doesn't exist any
+        // longer and we must not use it any more, so bail out immediately.
+        return;
+      }
+    }
+
     if( ExGlobals::GetExecuteCommand() )
     {
       ExGlobals::SetExecuteCommand( false );

commit 335e8f32c43b0ad982186f78c8465dd394cee640

Fix recent regression resulting in a crash after pausing scripts
The fix of 1a3bde3 (Don't use the script after all scripts were stopped,
2021-04-22) broke pausing the scripts because it incorrectly assumed
that NotInaScript() would only return true after stopping the scripts,
but it actually also returns true after pausing them.

Account for this now to ensure that we set the flag allowing us to
resume correctly before returning.

Fixes #10.

src/Parsing/Script.cpp
@@ -60,7 +60,11 @@ void Script::Run()
     if( !commandLine.empty() && commandLine!=wxString(wxT(" ")) )
     {
       ExGlobals::ProcessCommand(commandLine);
-      if ( ExGlobals::NotInaScript() )
+
+      // Note that calling PauseScripts() rather unexpectedly results in
+      // NotInaScript() returning true, but we do need to continue running this
+      // function in this case, if only to set scriptOriginatingPause_ to true.
+      if ( ExGlobals::NotInaScript() && !ExGlobals::GetPausingScript() )
       {
         // This can only happen if StopAllScripts() was called from inside the
         // command, in which case this script object itself doesn't exist any