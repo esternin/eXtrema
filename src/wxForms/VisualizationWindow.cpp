esternin/eXtrema

commit 98b6440b6be32a671f1712a538c1940184f46a2d

Use intermediate panels in all frames
This fixes problems with frame background showing through the place not
occupied by any controls under MSW and also fixes TAB navigation, which
doesn't work outside of a panel.

In most places, just creating a local wxPanel variable and using it as
the parent was enough. However in a couple of them, "this" of
wxFrame-derived type was used both as the window parent and as an
Extrema-specific variable and in these cases GetMainPanel() had to be
added, so that it could be used as the parent at wx level while still
using "this" for the other use.

src/wxForms/VisualizationWindow.cpp
@@ -170,15 +170,16 @@ VisualizationWindow::VisualizationWindow( wxWindow *parent )
   // proportion > 0    allows for vertical expansion
   // Expand()          allows for horizontal expansion
   
+  mainPanel_ = new wxPanel(this);
   wxBoxSizer *sizer = new wxBoxSizer( wxVERTICAL );
 
   visualizationSpeedButtonPanel_ = new VisualizationSpeedButtonPanel(this);
   sizer->Add( visualizationSpeedButtonPanel_, wxSizerFlags(0).Expand().Border(wxALL,1) );
 
-  notebook_ = new wxNotebook( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNB_RIGHT );
+  notebook_ = new wxNotebook( mainPanel_, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNB_RIGHT );
 
   sizer->Add( notebook_, wxSizerFlags(1).Expand().Border(wxALL,5) );
-  SetSizer( sizer );
+  mainPanel_->SetSizer( sizer );
 
   GraphicsPage *page = new GraphicsPage(notebook_);
 
@@ -785,7 +786,7 @@ BEGIN_EVENT_TABLE( MyStatusBar, wxStatusBar )
 END_EVENT_TABLE()
 
 MyStatusBar::MyStatusBar( VisualizationWindow *parent )
-    : wxStatusBar( (wxWindow*)parent, wxID_ANY )
+    : wxStatusBar( parent->GetMainPanel(), wxID_ANY )
 {
   unitsType_ = 0;
   units_[0] = wxT("graph units");

src/wxForms/AnalysisCommandPanel.cpp
@@ -27,7 +27,7 @@ BEGIN_EVENT_TABLE( AnalysisCommandPanel, wxPanel )
 END_EVENT_TABLE()
 
 AnalysisCommandPanel::AnalysisCommandPanel( AnalysisWindow *parent )
-    : wxPanel(parent,wxID_ANY,wxDefaultPosition,wxDefaultSize)
+    : wxPanel(parent->GetMainPanel(),wxID_ANY,wxDefaultPosition,wxDefaultSize)
 {
   // this panel will expand horizontally but not vertically
   //

src/wxForms/AnalysisMessagePanel.cpp
@@ -30,7 +30,7 @@ BEGIN_EVENT_TABLE( AnalysisMessagePanel, wxPanel )
 END_EVENT_TABLE()
 
 AnalysisMessagePanel::AnalysisMessagePanel( AnalysisWindow *parent )
-    : wxPanel(parent,-1,wxDefaultPosition,wxDefaultSize)
+    : wxPanel(parent->GetMainPanel(),-1,wxDefaultPosition,wxDefaultSize)
 {
   // this panel will expand both horizontally and vertically
   //

src/wxForms/AnalysisSpeedButtonPanel.cpp
@@ -40,7 +40,7 @@ BEGIN_EVENT_TABLE( AnalysisSpeedButtonPanel, wxPanel )
 END_EVENT_TABLE()
 
 AnalysisSpeedButtonPanel::AnalysisSpeedButtonPanel( AnalysisWindow *parent )
-    : wxPanel(parent,wxID_ANY,wxDefaultPosition,wxDefaultSize,wxRAISED_BORDER|wxTAB_TRAVERSAL),
+    : wxPanel(parent->GetMainPanel(),wxID_ANY,wxDefaultPosition,wxDefaultSize,wxRAISED_BORDER|wxTAB_TRAVERSAL),
       analysisWindow_(parent)
 {
   wxString imageDir = ExGlobals::GetImagePath();

src/wxForms/AnalysisWindow.cpp
@@ -117,13 +117,14 @@ AnalysisWindow::AnalysisWindow( extrema *extrema )
   // proportion > 0    allows for vertical expansion
   // Expand()          allows for horizontal expansion
   //
+  mainPanel_ = new wxPanel(this);
   wxBoxSizer *sizer = new wxBoxSizer( wxVERTICAL );
   sizer->Add( new AnalysisSpeedButtonPanel(this), wxSizerFlags(0).Expand().Border(wxALL,1) );
   messagePanel_ = new AnalysisMessagePanel(this);
   sizer->Add( messagePanel_, wxSizerFlags(1).Expand().Border(wxALL,1) );
   commandPanel_ = new AnalysisCommandPanel(this);
   sizer->Add( commandPanel_, wxSizerFlags(0).Expand().Border(wxALL,1) );
-  SetSizer( sizer );
+  mainPanel_->SetSizer( sizer );
 
   CreateStatusBar( 1 );
   SetStatusText( wxT("Welcome to eXtrema!") );

src/wxForms/AnalysisWindow.h
@@ -49,6 +49,8 @@ class AnalysisWindow : public wxFrame
 public:
   AnalysisWindow( extrema * );
   
+  wxPanel* GetMainPanel() const { return mainPanel_; }
+
   void WriteOutput( wxString const & );
   void ClearOutput();
   void AddCommandString( wxString const & );
@@ -100,6 +102,7 @@ public:
 
 private:
   extrema *extrema_;
+  wxPanel* mainPanel_;
   AnalysisMessagePanel *messagePanel_;
   AnalysisCommandPanel *commandPanel_;
   ReadVectorsForm *readVectorsForm_;

src/wxForms/AxisPopup.cpp
@@ -89,10 +89,11 @@ AxisPopup::AxisPopup( GraphicsPage *parent )
 
 void AxisPopup::CreateForm()
 {
+  wxPanel* const mainPanel = new wxPanel(this);
   wxBoxSizer *mainSizer = new wxBoxSizer( wxVERTICAL );
-  SetSizer( mainSizer );
+  mainPanel->SetSizer( mainSizer );
 
-  notebook_ = new wxNotebook( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNB_TOP );
+  notebook_ = new wxNotebook( mainPanel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNB_TOP );
   mainSizer->Add( notebook_, wxSizerFlags(0).Left().Border(wxALL,5) );
   //
   MakeAxisPanel();
@@ -101,7 +102,7 @@ void AxisPopup::CreateForm()
   MakeLocationPanel();
   MakeAxisScalesPanel();
   //
-  closeBTN_ = new wxButton( this, wxID_CLOSE, wxT("Close") );
+  closeBTN_ = new wxButton( mainPanel, wxID_CLOSE, wxT("Close") );
   mainSizer->Add( closeBTN_, wxSizerFlags(0).Center().Border(wxALL,2) );
 }
 

src/wxForms/ColorSelector.cpp
@@ -43,9 +43,10 @@ ColorSelector::ColorSelector( wxWindow *parent )
     : wxFrame(parent,wxID_ANY,wxT("color selector"),
               wxDefaultPosition,wxDefaultSize,wxDEFAULT_FRAME_STYLE)
 {
+  wxPanel* const mainPanel = new wxPanel(this);
   wxBoxSizer *mainSizer = new wxBoxSizer( wxVERTICAL );
   
-  wxPanel *topPanel = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNO_BORDER );
+  wxPanel *topPanel = new wxPanel( mainPanel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNO_BORDER );
   wxBoxSizer *topSizer = new wxBoxSizer( wxHORIZONTAL );
 
   colorMapGrid_ = new wxGrid( topPanel, ID_grid, wxDefaultPosition, wxDefaultSize, wxSUNKEN_BORDER );
@@ -63,7 +64,7 @@ ColorSelector::ColorSelector( wxWindow *parent )
   topPanel->SetSizer( topSizer );
   mainSizer->Add( topPanel, wxSizerFlags(1).Expand().Border(wxALL,1) );
   
-  wxPanel *bottomPanel = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNO_BORDER );
+  wxPanel *bottomPanel = new wxPanel( mainPanel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNO_BORDER );
   wxBoxSizer *bottomSizer = new wxBoxSizer( wxHORIZONTAL );
   wxButton *okButton = new wxButton( bottomPanel, wxID_OK, wxT("OK") );
   okButton->SetToolTip( wxT("accept the color as chosen above") );
@@ -74,7 +75,7 @@ ColorSelector::ColorSelector( wxWindow *parent )
   bottomPanel->SetSizer( bottomSizer );
   mainSizer->Add( bottomPanel, wxSizerFlags(1).Expand().Centre().Border(wxALL,1) );
 
-  SetSizer( mainSizer );
+  mainPanel->SetSizer( mainSizer );
   
   wxPersistentRegisterAndRestore(this, "ColorSelector");
 

commit 00e214a6cbdd594614dc61f8074b85995231e180

Fix status bar in the visualization window
This was broken by 98b6440 (Use intermediate panels in all frames,
2021-07-25) which used the wrong parent for creating the status bar: it
must be always child of the frame itself.

src/wxForms/VisualizationWindow.cpp
@@ -786,7 +786,7 @@ BEGIN_EVENT_TABLE( MyStatusBar, wxStatusBar )
 END_EVENT_TABLE()
 
 MyStatusBar::MyStatusBar( VisualizationWindow *parent )
-    : wxStatusBar( parent->GetMainPanel(), wxID_ANY )
+    : wxStatusBar( parent, wxID_ANY )
 {
   unitsType_ = 0;
   units_[0] = wxT("graph units");