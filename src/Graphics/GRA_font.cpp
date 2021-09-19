1d39c57378e0b54edf58d84f60fe7be16dccb9c2
Vadim Zeitlin <vadim@zeitlins.org>
2021-09-19 16:41:09 +0100

    Use the best available approximation for Extrema fonts
    Check if the font we want to use is available, try using "Free"-prefixed
    version (which corresponds to the names of the fonts distributed with
    Extrema) otherwise and, finally, if it's still not available, use the
    default font of the corresponding font family for the current platform.
    
    This should result in better font fidelity under Windows, both when
    FreeXXX fonts are available and when they are not.

--- src/Graphics/GRA_font.cpp
+++ src/Graphics/GRA_font.cpp
@@ -15,12 +15,55 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
-#include "wx/fontutil.h"
+#include "wx/fontenum.h"
 
 #include "GRA_font.h"
 
+namespace
+{
+
+// Return the font info using the available variant of the given portable font
+// name which must be one of "SANS", "SERIF" and "MONO".
+wxFontInfo GetAvailableVariantOf(wxString const& name)
+{
+  static wxArrayString const allFacenames = wxFontEnumerator::GetFacenames();
+
+  wxFontInfo info(12);
+
+  if ( allFacenames.Index(name, false /* case-insensitive */) != wxNOT_FOUND )
+  {
+    // This font is available, just use it directly.
+    return info.FaceName(name);
+  }
+
+  // Find an available alternative: start by the fonts that we install with
+  // Extrema that should be always available.
+  wxString nameAlt = "Free" + name;
+  if ( allFacenames.Index(nameAlt, false /* case-insensitive */) != wxNOT_FOUND )
+    return info.FaceName(nameAlt);
+
+  // If they aren't, just fall back to the corresponding default font of the
+  // given family for the current platform.
+  wxFontFamily family;
+  if ( name == "SANS" )
+    family = wxFONTFAMILY_SWISS;
+  else if ( name == "SERIF" )
+    family = wxFONTFAMILY_ROMAN;
+  else if ( name == "MONO" )
+    family = wxFONTFAMILY_TELETYPE;
+  else
+  {
+    wxFAIL_MSG("Unknown portable font name \"" + name + "\"");
+    return info;
+  }
+
+  return info.Family(family);
+}
+
+} // anonymous namespace
+
 GRA_font::GRA_font( wxString const &name ) 
-    : wxfont_(wxFontInfo(12).FaceName(name)),
+    : wxfont_(GetAvailableVariantOf(name)),
       name_( name )
 {
 }
