fd1aa317f95446dec19712a93a0da9d31fe02973
Edward Sternin <edward.sternin@brocku.ca>
2024-03-18 12:19:49 -0400

    was optional, now is required for gcc v13

--- src/Graphics/GRA_postscript.cpp
+++ src/Graphics/GRA_postscript.cpp
@@ -18,6 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <limits>
 #include <iomanip>
 #include <memory>
+#include <cstdint>
 #include <wx/gdicmn.h>
 
 #include "GRA_postscript.h"
